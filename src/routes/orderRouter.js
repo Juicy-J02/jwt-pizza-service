b5e711905998e78542a125a42f5a64cdf71b7d83
Jackson <jrgray609@gmail.com>
2026-04-09 22:28:03 -0600

    Pen Test

--- src/routes/orderRouter.js
+++ src/routes/orderRouter.js
@@ -100,7 +100,7 @@ orderRouter.post(
   authRouter.authenticateToken,
   asyncHandler(async (req, res) => {
     if (!req.body.items || !req.body.franchiseId || !req.body.storeId) {
-      return new StatusCodeError('bad request', 400);
+      throw new StatusCodeError('bad request', 400);
     }
     const orderReq = req.body;
     const order = await DB.addDinerOrder(req.user, orderReq);
